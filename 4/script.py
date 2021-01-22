b09872fd09ee730fe2636e3e6fab820b2b3882ea
Benedek Kiss <kissbenedekgabor@gmail.com>
2021-01-22 14:41:35 +0200

    Solve ex 4

--- 4/script.py
+++ 4/script.py
@@ -1,4 +1,12 @@
 import picamera
+from gpiozero import DistanceSensor
+from time import sleep
+sensor = DistanceSensor(23, 24)
 camera = picamera.PiCamera()
 camera.resolution= (640, 480)
-camera.capture("foo.jpeg")
+i = 0
+while True:
+    if sensor.distance < 1.0:
+        camera.capture("foo{i}.jpeg".format(i=i))
+        i += 1
+    sleep(1)
